0854837295460b2ff28907e6c324a758847bc92e
Mananas <Mananas@host.docker.internal>
2025-09-15 13:52:13 +0200

    frts

---
+++ src/main/java/cursogetafe/dixml/App.java
@@ -0,0 +1,13 @@
+package cursogetafe.dixml;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
